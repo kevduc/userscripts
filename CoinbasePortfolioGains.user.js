9fca3d39f0b6facf44dfca0d376a3aeca60b83e1
Kevin Duconge <64347790+kevduc@users.noreply.github.com>
2021-03-25 14:29:09 +0000

    Update CoinbasePortfolioGains.user.js
    - Local currency automatically detected
    - Cleaned and structured code
    - Better error handling

--- CoinbasePortfolioGains.user.js
+++ CoinbasePortfolioGains.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Coinbase Portfolio Gains
-// @version      1.1
+// @version      1.2.0
 // @description  Shows Coinbase portfolio gains
 // @author       KevDuc
 // @namespace    https://kevduc.github.io/
@@ -13,65 +13,179 @@
 // @run-at       document-end
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  ('use strict');
 
-    const totalInvestment = 0; // Change this to the total amount you invested (in your local currency)
+  const totalInvestment = 0; // Change this to the total amount you invested (in your local currency)
 
-    // Helper functions
-    const pause = (milli) => new Promise((resolve, reject) => setTimeout(resolve, milli));
+  // ----------------------------------------------------
+  // ----------------- Helper functions -----------------
+  // ----------------------------------------------------
 
-    const waitForTruthy = async (func, milli = 200) => {
-        let result;
-        while(!(result = func())) await pause(milli);
-        return result;
+  // Async tools
+
+  const pause = (milli) => new Promise((resolve, reject) => setTimeout(resolve, milli));
+
+  const waitForTruthy = async (func, milli = 200) => {
+    let result;
+    while (!(result = func())) await pause(milli);
+    return result;
+  };
+
+  document.querySelectorWhenLoaded = async (query) => await waitForTruthy(() => document.querySelector(query));
+
+  // Local Storage
+
+  const getLocalStorageArray = (keyName) => {
+    const string = localStorage.getItem(keyName);
+
+    const arrayDefault = [];
+    let array;
+
+    try {
+      array = JSON.parse(string) || [];
+    } catch (e) {
+      console.warn(`Error parsing "${string}":\n${e}`);
+      console.warn(`Cannot parse value of ${keyName} (local storage) as an array, defaulting to [${arrayDefault}].`);
+      array = arrayDefault;
     }
 
-    document.querySelectorWhenLoaded = async (query) => await waitForTruthy(() => document.querySelector(query));
+    return array;
+  };
 
-    // -------------------------
+  const setLocalStorageArray = (keyName, array) => localStorage.setItem(keyName, JSON.stringify(array));
 
-    let totalInvestmentHistory = JSON.parse(localStorage.getItem('totalInvestmentHistory'));
+  // Locale Support
 
-    // No previous investment
-    if (totalInvestmentHistory === null) totalInvestmentHistory = [];
+  // From https://stackoverflow.com/a/42213804, credit to naitsirch
+  const parseLocaleFloat = (string, locale) => {
+    const thousandSeparator = (1111).toLocaleString(locale).replace(/\p{Number}/gu, '');
+    const decimalSeparator = (1.1).toLocaleString(locale).replace(/\p{Number}/gu, '');
 
-    const previousTotalInvestment = totalInvestmentHistory[0];
+    return parseFloat(
+      string.replace(new RegExp('\\' + thousandSeparator, 'g'), '').replace(new RegExp('\\' + decimalSeparator), '.')
+    );
+  };
 
-    // totalInvestment value changed
-    if (totalInvestment !== 0 && totalInvestment !== previousTotalInvestment) {
-        totalInvestmentHistory.unshift(totalInvestment);
-        localStorage.setItem('totalInvestmentHistory', JSON.stringify(totalInvestmentHistory));
+  // ----------------------------------------------------
+  // -------------------- Investment --------------------
+  // ----------------------------------------------------
+
+  class TotalInvestmentHistory {
+    constructor() {}
+
+    static getInstance() {
+      if (!TotalInvestmentHistory.instance) {
+        TotalInvestmentHistory.instance = new TotalInvestmentHistory();
+        // Get the list of previous investments (empty array if none)
+        TotalInvestmentHistory.instance.history = getLocalStorageArray('totalInvestmentHistory');
+      }
+
+      return TotalInvestmentHistory.instance;
     }
 
-    const invested = totalInvestmentHistory[0] || 0;
-
-    function updateROI(balance, roi) {
-        const value = parseFloat(balance.innerText.replace(/[$€£,]/g,''));
-        const profit = value - invested;
-        roi.style.color = profit > 0 ? 'green' : 'red';
-        roi.innerText = `${profit > 0 ? '+' : '-'}£${Math.abs(profit).toFixed(2)} (${(100*profit/invested).toFixed(2)}%)`;
+    // Get the latest total investment value from the history (null if none)
+    latest() {
+      return this.history[0] || null;
     }
 
-    async function init() {
-        const balance = await document.querySelectorWhenLoaded('h1[class*="Balance__BalanceHeader"]');
+    update(totalInvestment) {
+      // Get the last investement value
+      const lastTotalInvestment = this.latest();
 
-        const roi = document.createElement('h1');
-        roi.className = balance.className;
-        roi.style = `display:inline-block; font-size:x-large`;
-        roi.id = "balanceROI-tampermonkey";
-
-        balance.style.display = 'inline-block';
-        balance.insertAdjacentElement('afterend', roi);
-
-        const balanceTextNode = balance.firstChild;
-
-        const update = () => updateROI(balance, roi);
-        const observer = new MutationObserver(update);
-        observer.observe(balanceTextNode, { characterData: true });
-
-        update();
+      // Total investment value has changed
+      if (totalInvestment !== 0 && totalInvestment !== lastTotalInvestment) {
+        // Add the new value to the history
+        this.history.unshift(totalInvestment);
+        // Save the new history
+        setLocalStorageArray('totalInvestmentHistory', this.history);
+      }
     }
+  }
 
-    init();
+  const totalInvestmentHistory = TotalInvestmentHistory.getInstance();
+  totalInvestmentHistory.update(totalInvestment);
+
+  const invested = totalInvestmentHistory.latest() || 0;
+
+  // ----------------------------------------------------
+  // --------------------- Balance ----------------------
+  // ----------------------------------------------------
+
+  const balanceValueRegex = /(?:[,\.]?\p{Number}+)+/gu;
+
+  const getBalanceValueStr = (balanceElement) => {
+    const balanceText = balanceElement.innerText;
+    const balanceValueStr = balanceText.match(balanceValueRegex);
+    if (balanceValueStr === null) throw new Error(`Cannot read balance value from ${balanceText}.`);
+    return balanceValueStr[0];
+  };
+
+  const getBalanceValue = (balanceElement) => {
+    const balanceValueStr = getBalanceValueStr(balanceElement);
+    const balanceValue = parseLocaleFloat(balanceValueStr); // undefined locale means use computer default
+    if (isNaN(balanceValue)) throw new Error(`Cannot parse balance value "${balanceValueStr}" to a float.`);
+    return balanceValue;
+  };
+
+  let applyBalanceCurrencyTemplate = (value) => value; // By default, return value unchanged
+
+  const updateBalanceCurrencyTemplate = (balanceElement) => {
+    applyBalanceCurrencyTemplate = (value) => balanceElement.innerText.replace(balanceValueRegex, `${value}`);
+  };
+
+  // ----------------------------------------------------
+  // ---------------------- Profit ----------------------
+  // ----------------------------------------------------
+
+  const formatProfit = (profit) =>
+    `${profit > 0 ? '' : '-'}${applyBalanceCurrencyTemplate(
+      // undefined locale means use computer default
+      Math.abs(profit).toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      })
+    )}`;
+
+  const formatProfitPercent = (profitPercent) => `${profitPercent > 0 ? '+' : '-'}${Math.abs(profitPercent).toFixed(2)}%`;
+
+  const updateProfit = (profitElement, balanceValue) => {
+    const profit = balanceValue - invested;
+    const profitPercent = (100 * profit) / invested;
+    const profitColor = profit > 0 ? 'green' : 'red';
+    profitElement.style.color = profitColor;
+    profitElement.innerText = `${formatProfitPercent(profitPercent)} (${formatProfit(profit)})`;
+  };
+
+  const createProfitElement = (balanceElement) => {
+    const profitElement = document.createElement('h2');
+    profitElement.className = balanceElement.className;
+    profitElement.style = `display:inline-block; font-size:20px`;
+
+    balanceElement.style.display = 'inline-block';
+    balanceElement.insertAdjacentElement('afterend', profitElement);
+
+    return profitElement;
+  };
+
+  // ----------------------------------------------------
+  // -------------------- Initialize --------------------
+  // ----------------------------------------------------
+
+  async function init() {
+    const balanceElement = await document.querySelectorWhenLoaded('h1[class*="Balance__BalanceHeader"]');
+    const balanceTextNode = balanceElement.firstChild;
+
+    updateBalanceCurrencyTemplate(balanceElement);
+
+    const profitElement = createProfitElement(balanceElement);
+
+    const update = () => updateProfit(profitElement, getBalanceValue(balanceElement));
+    const observer = new MutationObserver(update);
+    observer.observe(balanceTextNode, { characterData: true });
+
+    update();
+  }
+
+  init();
 })();
